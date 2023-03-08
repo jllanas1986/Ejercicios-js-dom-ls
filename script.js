665bf428b329730ad21b0d4563eb5c6544875e88
Jose Llanas <josellanasmota86@gmail.com>
2023-03-08 11:36:15 +0100

    ultimo ejercicio funciones

--- script.js
+++ script.js
@@ -208,12 +208,91 @@ console.log(animalesHerbivoros);
 //Crea un array con los animales que son omnívoros o aves y menores de 3 años. No utilices el método filter de las arrays y muéstralo por consola.
 let omnivorosOaves = []
 for (const animal of animales) {
-    if (animal.especie === '' && animal.edad % 2 === 0) {
-        animalesHerbivoros.push(animal);   
+    if (animal.especie === 'Omnívoro' || animal.especie === 'Ave') {
+        omnivorosOaves.push(animal);   
     }
 }
-console.log(animalesHerbivoros);
+console.log(omnivorosOaves);
 
 //Crea un array con los animales que su especie empieza por "H". No utilices el método filter de las arrays y muéstralo por consola.
+let animalsH = []
+for (const animal of animales) {
+    if (animal.especie.startsWith('H')) {
+        animalsH.push(animal);
+    }
+}
+console.log(animalsH);
 
 //Crea un array con los animales cuyo nombre tenga más de 4 letras y su especie sea felino o herbívoro. No utilices el método filter de las arrays y muéstralo por consola.
+let animalsFourLetters = []
+for (const animal of animales) {
+    if (animal.nombre.length > 4) {
+        animalsFourLetters.push(animal)
+    }
+}
+console.log(animalsFourLetters);
+
+//Crea una función llamada eliminarDuplicados(array) que reciba como parámetro un array de elementos y devuelva un nuevo array con los elementos únicos, es decir, sin elementos duplicados. Puedes utilizar un ciclo for...of para recorrer el array original.
+function eliminarDuplicados(arr) {
+  let arrNoRepeat = []
+    for (let contador of arr) {
+        if (!arrNoRepeat.includes(contador)) {
+            arrNoRepeat.push(contador)
+        }
+    }
+    return arrNoRepeat
+}
+console.log(eliminarDuplicados([1, 2, 3, 3, 4, 5, 5, 6])); // Devuelve [1, 2, 3, 4, 5, 6]
+console.log(eliminarDuplicados(["hola", "adios", "hola", "hasta", "luego"])); // Devuelve ["hola", "adios", "hasta", "luego"]
+
+//Crea una función llamada sumarElementos(array) que reciba como parámetro un array de números y devuelva la suma de todos sus elementos. Puedes utilizar un ciclo for...of para recorrer el array y sumar sus elementos
+function sumarElementos(arr) {
+  let array = [];
+  for (const numero of arr) {
+    array.push(numero)
+  }
+  let total = 0;
+  for (const contador of array) {
+    total += contador
+  }
+  return total;
+}
+console.log(sumarElementos([1, 2, 3, 4, 5])); // Devuelve 15
+console.log(sumarElementos([-5, 10, -15, 20])); // Devuelve 10
+
+//Crea una función en contarCaracteres(arroy) que reciba como parámetro un array de cadenas de caracteres y devuelva la cantidad total de caracteres que hay en todas las cadenas juntas. Puedes utilizar un ciclo for...of para recorrer el array y contar los caracteres.
+function contarCaracteres(arr) {
+  let suma = 0;
+  for (const string of arr) {
+    suma += string.length
+  }
+  return suma
+}
+console.log(contarCaracteres(["Hola", "Mundo"])); // Devuelve 9
+console.log(contarCaracteres(["JavaScript", "es", "genial"])); // Devuelve 18
+
+//Crea una función llamada numeroMasGrande(array) que reciba como parámetro un array de números y devuelva el número más grande de ese array. Puedes utilizar un ciclo for...of para recorrer el array y condicionales para encontrar el número más grande.
+function numeroMasGrande(arr) {
+  let numMayor = 0;
+  for (const num of arr) {
+    if (num >= numMayor) {
+      numMayor = num
+    }
+  }
+  return numMayor
+}
+
+console.log(numeroMasGrande([5,7,88,26,42]));
+
+//Crea una función llamada cadenaMasLarga(array) que reciba como parámetro un array de cadenas de caracteres y devuelva la cadena más larga de ese array. Puedes utilizar un ciclo for...of para recorrer el array y condicionales para encontrar la cadena más larga.
+function cadenaMasLarga(arr) {
+  let stringLarga = '';
+  for (const string of arr) {
+    if (string.length >= stringLarga) {
+      stringLarga = string
+    }
+  }
+  return stringLarga
+}
+console.log(cadenaMasLarga(["Hola", "Mundo"])); // Devuelve "Mundo"
+console.log(cadenaMasLarga(["JavaScript", "es", "genial"])); // Devuelve "JavaScript"
